2170a689f5b2b38a8ae752e15766f6f2b2196984
Alok963-ai <alokjangde6@gmail.com>
2025-06-17 10:39:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,12 +3,12 @@ import os
 ##Code Written By @ItsMeMaster
 
 class Config(object):
-    BOT_TOKEN = "7964228633:AAHeNP6DRavUKNOwq4Kyj8cPJA6pzNH3fug"
+    BOT_TOKEN = "7fug"
     DB_NAME = "alokjangde6"
     API_ID = 20288951
     API_HASH = "e8cb5fb7a475b5f5eb3b0ef0e6ca03a8"
     ADMIN_ID = [7833842279]
-    DB_URL = "mongodb+srv://alokjangde6:Sapnaipali71$@cluster0.cx60pkr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    DB_URL = "mongodb+srv://alokjangde"
     LOG_CHANNEL = -1002548493129 # Your Log Channel ID (Bot ko ADMIN BNAYE)
     USERLINK = "t.me/Alliswell71"
     TUTORIAL_VIDEO = ""
