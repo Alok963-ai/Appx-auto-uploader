a8f8377df2d014eaa8bf320f08c17684753ca327
Alok963-ai <alokjangde6@gmail.com>
2025-06-17 09:40:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 ##Code Written By @ItsMeMaster
 
 class Config(object):
-    BOT_TOKEN = ""
+    BOT_TOKEN = "7964228633:AAHeNP6DRavUKNOwq4Kyj8cPJA6pzNH3fug"
     DB_NAME = "alokjangde6"
     API_ID = 20288951
     API_HASH = "e8cb5fb7a475b5f5eb3b0ef0e6ca03a8"
