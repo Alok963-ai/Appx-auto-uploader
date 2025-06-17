bb0493af60ed531d8fedcd1154e4229b61fe309d
Alok963-ai <alokjangde6@gmail.com>
2025-06-17 09:29:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,6 +9,6 @@ class Config(object):
     API_HASH = "e8cb5fb7a475b5f5eb3b0ef0e6ca03a8"
     ADMIN_ID = [7833842279]
     DB_URL = "mongodb+srv://alokjangde6:Sapnaipali71$@cluster0.cx60pkr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-    LOG_CHANNEL = 2548493129 # Your Log Channel ID (Bot ko ADMIN BNAYE)
+    LOG_CHANNEL = -1002548493129 # Your Log Channel ID (Bot ko ADMIN BNAYE)
     USERLINK = "t.me/Alliswell71"
     TUTORIAL_VIDEO = ""
